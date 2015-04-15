79740d74a6d8d5d6073e1a839ddbee1e87ff242f
abjia <daohao1914>
2015-04-15 10:42:15 +0800

    解决滚动问题

--- js/app.js
+++ js/app.js
@@ -213,7 +213,7 @@ PointMall.run(function ($ionicPlatform) {
     //版本控制
     .constant("VERSION",{
 
-        URL_VERSION : "7.2",
+        URL_VERSION : "7.5",
         ADDRESS_SOURCE_VERSION : "2.2"
 
     });
