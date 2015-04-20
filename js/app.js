7e1e13dd13798aa80e14b67ef2176d8a9c41e304
abjia <daohao1914>
2015-04-20 19:10:11 +0800

    修改一些图片文字

--- js/app.js
+++ js/app.js
@@ -216,7 +216,7 @@ PointMall.run(function ($ionicPlatform) {
     //版本控制
     .constant("VERSION",{
 
-        URL_VERSION : "7.5",
+        URL_VERSION : "8.1",
         ADDRESS_SOURCE_VERSION : "2.2"
 
     });
